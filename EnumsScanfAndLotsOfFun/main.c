02bbe13571f3519df033712f365708b35352c585
dmath6 <dylanmatheson484@gmail.com>
2019-05-02 19:45:59 +1000

    Just an update and added another WIP

---
+++ EnumsScanfAndLotsOfFun/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()  //Not done yet!!
+{
+    char FirstName;
+    char SecondName;
+    char ThirdName;
+    char FourthName;
+    enum Color{Red,Blue,Yellow,Green};
+    enum Sex{Male,Female,Other};
+    enum Age{Over18,Over10};
+    printf("Please enter your first name");
+    scanf('Firstname'); //This will be done (hopefully)
+    printf("Hello world!\n");
+    return 0;
+}
