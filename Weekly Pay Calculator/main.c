005f83cebf9bb5bb20cef3245016a51ed292c5f1
dmath6 <dylanmatheson484@gmail.com>
2019-05-18 15:10:16 +0800

    Fixed it

--- Weekly Pay Calculator/main.c
+++ Weekly Pay Calculator/main.c
@@ -1,45 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define PAYRATE 12.00
+#define TAXRATE_300 .15
+#define TAXRATE_150 .20
+#define TAXRATE_REST .25
+#define OVERTIME 40
 int main()
 {
-    float hours;
-    float grosspay;
-    float netpay;
-    float aftertax;
-    float tax;
-    //Currently static variables
-    float payrateperhour= 44;
+    int hours;
+    double grosspay;
+    double taxes;
 
-    printf(" \n Please enter number of hours worked\n");
-    scanf(" \n %f", &hours);
-    printf(" \n You have entered %f hours\n ", hours);
-    //Work out the total amount
-    grosspay= hours * payrateperhour;
-    printf(" \n Your total earned for this period is %f dollers\n", grosspay);
+    printf("Please enter hours worked for this week: ");
 
-    if (hours > 40)
+    scanf("%d", &hours);
+
+    if (hours  <= 40)
     {
-        grosspay = grosspay * 1.5;
-        printf("\n The amount you earned over time and a half is %f\n", netpay);
+        grosspay = hours * PAYRATE;
+    }
+    else
+    {
+        grosspay = 40 * PAYRATE;
+        double overtimepay = (hours * 40) * (PAYRATE * 1.5);
+        grosspay += overtimepay;
     }
 
-    if (grosspay > 300)
-    {
-        netpay = aftertax * 0.15;
-    }
 
-    else if (grosspay > 450);
-    {
-        netpay = aftertax * 0.20;
-    }
+    if (grosspay <= 300)
+        {
+            taxes = grosspay * TAXRATE_300;
+        }
+    else if(grosspay > 300 && grosspay <= 450)
+        {
+            taxes = 300 * TAXRATE_300;
+            taxes += (grosspay - 300) * TAXRATE_150;
+        }
 
-        tax = netpay-aftertax;
+    else if (grosspay > 450)
+        {
+            taxes = 300 * TAXRATE_300;
+            taxes += 150 * TAXRATE_150;
+            taxes += (grosspay - 450) * TAXRATE_REST;
+        }
 
-    if (aftertax != 0);
-    {
-        printf("\nAfter tax is %f\n", tax);
-    }
+        double netpay = grosspay - taxes;
 
+        printf("Your gross pay this week is: %.2f\n", grosspay);
+        printf("Your taxes this week is: %.2f\n", taxes);
+        printf("Your net pay week is: %.2f\n", netpay);
     return 0;
 }
